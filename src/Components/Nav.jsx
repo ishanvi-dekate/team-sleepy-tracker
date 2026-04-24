ae74bea37023684f149b5e8313256db6a3ef030b
aKarthik25 <karthika2870@my.sduhsd.net>
2026-04-24 13:31:07 -0700

    Finished header, edited navigation bar

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -8,6 +8,11 @@ function Nav({ setPage }) {
         <ul>
           <li onClick={() => setPage("login")}>Login</li>
           <li onClick={() => setPage("home")}>Home</li>
+          <li onClick={() => setPage("todo")}>To-Do</li>
+          <li onClick={() => setPage("mental")}>Mental Stae</li>
+          <li onClick={() => setPage("settings")}>Settings</li>
+          <li onClick= {() => setPage("profile")}>Profile</li>
+          <li onClick= {()=> setPage("sign-out")}>Sign-Out</li>
         </ul>
       </div>
     </footer>
