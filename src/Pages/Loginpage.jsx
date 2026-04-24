cef5419e7255bc37f5abdc5bb985eb28d6dac944
Nathan-Zhao0806 <zhaon2273@my.sduhsd.net>
2026-04-24 13:31:51 -0700

    finish the login page

---
+++ src/Pages/Loginpage.jsx
@@ -0,0 +1,13 @@
+import { beforeAuthStateChanged } from "firebase/auth/cordova";
+import Login from "../Components/Login";
+import './Login.css';
+function Loginpage() {
+    <>
+<p className="app-title">efficient.epp</p>
+<p className="app-title">expLore YoUr HaBits</p>
+<p className="app-subtitle">Get in charge of your assignments, activities and sleep!</p>
+<></>
+<div class="login"><Login /></div>
+    </>
+}
+export default Loginpage
